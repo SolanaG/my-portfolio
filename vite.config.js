d76f47d50f99baeed85bd05be399984f4a35f4b2
SolanaG <gomez.solanarocio@gmail.com>
2023-04-04 15:46:11 -0300

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({ fastRefresh: false })],
   worker: {
     plugins: [react()],
+  },
+  build: {
+    rollupOptions: {
+      main: resolve(__dirname, "index.html"),
+    },
   },
 });
